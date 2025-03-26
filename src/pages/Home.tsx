c6d4edc1b7af9eb77375747812816bf779671929
Carlos Santos <carlos22santos03@gmail.com>
2025-03-26 20:45:41 -0300

    Feat: Avatar Lists Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,35 @@
-import { Header, CardComp } from '@/components'
+import { AvatarsList, Header, CardComp } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      name: 'Dnc',
+      subtitle: currencyConverter(1000),
+      avatar: '/dnc-avatar.svg',
+    },
+    {
+      name: 'Dnc',
+      subtitle: currencyConverter(2000),
+      avatar: '/dnc-avatar.svg',
+    },
+    {
+      name: 'Dnc',
+      subtitle: currencyConverter(3000),
+      avatar: '/dnc-avatar.svg',
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComp>
           <h1>Home</h1>
+        </CardComp>
+
+        <CardComp>
+          <AvatarsList listData={mockListData} />
         </CardComp>
       </Container>
     </>
